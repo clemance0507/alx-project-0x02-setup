23fe76022fc5ff4070aec74f827bed5a3165d44a
Clemence Mizero Uwase <cmizero@eucl.reg.rw>
2025-08-18 12:18:42 +0200

    first commit

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Welcome to My Next.js App 🚀
+      </h1>
+    </div>
+  );
+}
