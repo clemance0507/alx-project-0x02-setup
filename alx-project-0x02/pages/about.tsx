13eb4120192f57db5a6c25ec7b1fc07994f3eca2
Clemence Mizero Uwase <cmizero@eucl.reg.rw>
2025-08-18 16:33:17 +0200

    excercise no5

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Button from "@/components/common/Button";
 
 export default function AboutPage() {
   return (
-    <div className="flex items-center justify-center h-screen bg-yellow-100">
-      <h1 className="text-4xl font-bold text-yellow-800">This is the About Page 📖</h1>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-6">
+      <h1 className="text-3xl font-bold mb-6">About Page 👋</h1>
+
+      <div className="flex gap-4">
+        <Button label="Small Button" size="small" shape="rounded-sm" />
+        <Button label="Medium Button" size="medium" shape="rounded-md" />
+        <Button label="Large Button" size="large" shape="rounded-full" />
+      </div>
+    </main>
   );
 }
